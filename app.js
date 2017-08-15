7057b75ca0059de0b2d4d9b72dd7d916aa5918f4
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-15 19:01:28 -0400

    inital commit, work in progress. page populate with li data, need to add checked boxes and line out styling to the first three items

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+//Listening on root
+app.get('/todo/', function (req, res) {
+  res.render('todo',
+   { "toDoList" : [
+     { "list" : 'Learn Node Basics' },
+     { "list" : 'Learn Express Basics' },
+     { "list" : 'Learn Mustache' },
+     { "list" : 'Learn HTML forms with Express' },
+     { "list" : 'Learn about authentication' },
+     { "list" : 'Learn how to connect to PostgreSQL' },
+     { "list" : 'Learn how to create databases' },
+     { "list" : 'Learn SQL' },
+     { "list" : 'Learn how to connect to PostgreSQL from Node' },
+     { "list" : 'Learn how to use Sequelize' }
+   ]
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
